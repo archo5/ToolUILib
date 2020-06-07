8fc3e9347340f16eb2e23885b05c49a1df68547e
snake5 <snake5creator@gmail.com>
2020-06-08 02:21:43 +0300

    fret: more image formats

--- fret/ImageParsers.cpp
+++ fret/ImageParsers.cpp
@@ -12,6 +12,14 @@ static uint32_t RGB5A1_to_RGBA8(uint16_t v)
 	return (r << 0) | (g << 8) | (b << 16) | (a << 24);
 }
 
+static uint32_t RGB565_to_RGBA8(uint16_t v)
+{
+	uint32_t b = ((v >> 0) & 0x1f) << 3;
+	uint32_t g = ((v >> 5) & 0x3f) << 2;
+	uint32_t r = ((v >> 11) & 0x1f) << 3;
+	return (r << 0) | (g << 8) | (b << 16) | (0xffUL << 24);
+}
+
 
 struct ReadImageIO
 {
@@ -51,6 +59,45 @@ static void ReadImage_RGBo8(ReadImageIO& io, const ImageInfo& info)
 	for (uint32_t px = 0; px < info.width * info.height; px++)
 	{
 		io.bytes[px * 4 + 3] = io.bytes[px * 4 + 3] ? 0xff : 0;
+	}
+}
+
+static void ReadImage_G8(ReadImageIO& io, const ImageInfo& info)
+{
+	if (info.width > 4096)
+	{
+		io.error = true;
+		return;
+	}
+
+	uint8_t tmp[4096];
+	for (uint32_t y = 0; y < info.height; y++)
+	{
+		io.ds->Read(info.offImg + y * info.width, info.width, tmp);
+		for (uint32_t x = 0; x < info.width; x++)
+			io.pixels[y * info.width + x] = tmp[x] | (tmp[x] << 8) | (tmp[x] << 16) | 0xff000000UL;
+	}
+}
+
+static void ReadImage_8BPP_RGBA8(ReadImageIO& io, const ImageInfo& info)
+{
+	if (info.width > 4096)
+	{
+		io.error = true;
+		return;
+	}
+
+	uint32_t pal[256];
+	io.ds->Read(info.offPal, 256 * 4, pal);
+
+	uint8_t line[4096];
+	for (uint32_t y = 0; y < info.height; y++)
+	{
+		io.ds->Read(info.offImg + info.width * y, info.width, line);
+		for (uint32_t x = 0; x < info.width; x++)
+		{
+			io.pixels[x + y * info.width] = pal[line[x]];
+		}
 	}
 }
 
@@ -134,15 +181,80 @@ static void ReadImage_4BPP_RGBo8(ReadImageIO& io, const ImageInfo& info)
 	}
 }
 
+struct DXT1Block
+{
+	uint16_t c0, c1;
+	uint32_t pixels;
+
+	uint32_t GetColor(int x, int y)
+	{
+		uint32_t c[4] = { RGB565_to_RGBA8(c0), RGB565_to_RGBA8(c1) };
+		if (c0 > c1)
+		{
+			uint8_t r0 = (c[0] >> 0) & 0xff;
+			uint8_t g0 = (c[0] >> 8) & 0xff;
+			uint8_t b0 = (c[0] >> 16) & 0xff;
+			uint8_t r1 = (c[1] >> 0) & 0xff;
+			uint8_t g1 = (c[1] >> 8) & 0xff;
+			uint8_t b1 = (c[1] >> 16) & 0xff;
+			uint8_t r2 = r0 * 2 / 3 + r1 / 3;
+			uint8_t g2 = g0 * 2 / 3 + g1 / 3;
+			uint8_t b2 = b0 * 2 / 3 + b1 / 3;
+			uint8_t r3 = r0 / 3 + r1 * 2 / 3;
+			uint8_t g3 = g0 / 3 + g1 * 2 / 3;
+			uint8_t b3 = b0 / 3 + b1 * 2 / 3;
+			c[2] = r2 | (g2 << 8) | (b2 << 16) | 0xff000000UL;
+			c[3] = r3 | (g3 << 8) | (b3 << 16) | 0xff000000UL;
+		}
+		else
+		{
+			c[2] = (c[0] >> 1) + (c[1] >> 1);
+			c[3] = 0;
+		}
+
+		int at = y * 4 + x;
+		int idx = (pixels >> (at * 2)) & 0x3;
+		return c[idx];
+	}
+};
+
+static void ReadImage_DXT1(ReadImageIO& io, const ImageInfo& info)
+{
+	uint32_t nbx = info.width / 4;
+	uint32_t nby = info.height / 4;
+
+	uint64_t at = info.offImg;
+	for (uint32_t by = 0; by < nby; by++)
+	{
+		for (uint32_t bx = 0; bx < nbx; bx++)
+		{
+			DXT1Block block;
+			io.ds->Read(at, sizeof(block), &block);
+			at += sizeof(block);
+
+			for (uint32_t y = by * 4; y < std::min((by + 1) * 4, info.height); y++)
+			{
+				for (uint32_t x = bx * 4; x < std::min((bx + 1) * 4, info.width); x++)
+				{
+					io.pixels[x + info.width * y] = block.GetColor(x - bx * 4, y - by * 4);
+				}
+			}
+		}
+	}
+}
+
 
 static const ImageFormat g_imageFormats[] =
 {
 	{ "Basic", "RGBA8", ReadImage_RGBA8 },
 	{ "Basic", "RGBX8", ReadImage_RGBX8 },
 	{ "Basic", "RGBo8", ReadImage_RGBo8 },
+	{ "Basic", "G8", ReadImage_G8 },
+	{ "Basic", "8BPP_RGBA8", ReadImage_8BPP_RGBA8 },
 	{ "PSX", "RGB5A1", ReadImage_RGB5A1 },
 	{ "PSX", "4BPP_RGB5A1", ReadImage_4BPP_RGB5A1 },
 	{ "PSX", "4BPP_RGBo8", ReadImage_4BPP_RGBo8 },
+	{ "S3TC", "DXT1", ReadImage_DXT1 },
 };
 
 
